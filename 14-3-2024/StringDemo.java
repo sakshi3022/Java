301d33af49bdc35a33fdf8275b44c46ad5c9a961
Sakshi shinde <103052042+sakshi3022@users.noreply.github.com>
2024-03-15 12:23:50 +0530

    Done

---
+++ 14-3-2024/StringDemo.java
@@ -0,0 +1,19 @@
+class StringDemo {
+      
+    public static void main (String[] args){
+
+        //There are two ways to create string
+
+        //First way:
+
+        String name = "Sakshi";
+        name = "Shinde";
+        System.out.println(name);
+
+        //Second way:
+        String name1 = new String("Shreya");
+        System.out.println(name1);
+
+        
+    }
+}
---
+++ 14-3-2024/StringMethod.java
@@ -0,0 +1,17 @@
+class StringMethod {
+    
+    public static void main (String[] args){
+
+        String name = "Sakshi shreya dhanshree Atharav";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+
+        // String name1 = new String("Sakshi");
+        // String name2 = new String("Sakshi");
+
+        String name1 = "Sakshi";
+        String name2 = "Sakshi";
+
+        System.out.println(name1 == name2);
+    }
+}
